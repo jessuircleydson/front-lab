f759a6ef26885f27ad4c3e73d96f97a560d1665f
Tiago Fuelber <tfuelber@gmail.com>
2016-12-15 23:34:48 -0200

    update gulpfile and package.json

--- estudo1/Gulp/Exemplos/TiagoFuelber/gulpfile.js
+++ estudo1/Gulp/Exemplos/TiagoFuelber/gulpfile.js
@@ -1,9 +1,21 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['task1'], function() {
+gulp.task('default', ['task1', 'sass', 'watch'], function() {
 
 });
 
 gulp.task('task1', function() {
   console.log('Hello World');
+});
+
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./*.scss', ['sass']);
 });
